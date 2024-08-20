05a1f609e94dce67d4ba491e134e4f72bf3bb7e1
Jiten Rai <raijiten96@gmail.com>
2024-08-20 08:27:50 +0545

    confidentials removed

--- insta_auto.py
+++ insta_auto.py
@@ -1,6 +1,6 @@
-from instabot import Bot
-bot = Bot()
-bot.login(username = 'jitenrai_jkr21', password = "inezmybaebaeemama<3..", use_cookie = False)
-bot.follow('seuls_les_braves')
-# # bot.upload_photo("", caption="")
-bot.send_message('I loveeeeeeee youuuuuuuuuu \n And This message was sent to you by python code. \n Fancy banauna english ma lekheko...\n huhuhuh.. emojis chahi xaina nita..', ['seuls_les_braves'])
+# from instabot import Bot
+# bot = Bot()
+# bot.login(username = 'jitenrai_jkr21', password = "inezmybaebaeemama<3..", use_cookie = False)
+# bot.follow('seuls_les_braves')
+# # # bot.upload_photo("", caption="")
+# bot.send_message('I loveeeeeeee youuuuuuuuuu \n And This message was sent to you by python code. \n Fancy banauna english ma lekheko...\n huhuhuh.. emojis chahi xaina nita..', ['seuls_les_braves'])
